f956ae9bf7da25ed39c1b65a991bec9e1e41e4d3
Shahil <shahil1.jha@gmail.com>
2021-03-01 19:45:49 +0545

    pkg add: screenutil, shared preference and secure storage

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Summer Project',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
