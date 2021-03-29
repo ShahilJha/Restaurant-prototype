d56f458828cb4b5ae0bbedb32bcc8ca0d289b846
Shahil <shahil1.jha@gmail.com>
2021-03-29 16:13:55 +0545

    bug: login scroll view

--- lib/screens/common/login/login_screen.dart
+++ lib/screens/common/login/login_screen.dart
@@ -9,40 +9,47 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(
-            'assets/images/logo.png',
-            height: 500.w,
-            width: 500.w,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: 2280.h,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logo.png',
+                height: 500.w,
+                width: 500.w,
+              ),
+              AppTextField(hint: 'USERNAME'),
+              AppTextField(hint: 'PASSWORD'),
+              OccupationChips(),
+              AppButton(
+                text: 'Log-in',
+                onPressed: () {
+                  // Navigator.pop(context);
+                  Navigator.of(context)
+                      .pushNamed('/waiter_running_order_screen');
+                },
+              ),
+              AppButton(
+                text: 'Log-in',
+                onPressed: () {
+                  // Navigator.pop(context);
+                  Navigator.of(context).pushNamed('/kitchen_running_orders');
+                },
+              ),
+              AppButton(
+                text: 'Log-in',
+                onPressed: () {
+                  // Navigator.pop(context);
+                  Navigator.of(context)
+                      .pushNamed('/receptionist_running_orders');
+                },
+              ),
+            ],
           ),
-          AppTextField(hint: 'USERNAME'),
-          AppTextField(hint: 'PASSWORD'),
-          OccupationChips(),
-          AppButton(
-            text: 'Log-in',
-            onPressed: () {
-              // Navigator.pop(context);
-              Navigator.of(context).pushNamed('/waiter_running_order_screen');
-            },
-          ),
-          AppButton(
-            text: 'Log-in',
-            onPressed: () {
-              // Navigator.pop(context);
-              Navigator.of(context).pushNamed('/kitchen_running_orders');
-            },
-          ),
-          AppButton(
-            text: 'Log-in',
-            onPressed: () {
-              // Navigator.pop(context);
-              Navigator.of(context).pushNamed('/receptionist_running_orders');
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
