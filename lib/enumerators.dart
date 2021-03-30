f72adcba5944a409cd37b26ba0a1a8582434ec84
Shahil <shahil1.jha@gmail.com>
2021-03-30 19:36:04 +0545

    enum: add

--- lib/enumerators.dart
+++ lib/enumerators.dart
@@ -1,5 +1,19 @@
 enum Position {
-  waiter,
-  receptionist,
-  kitchenStaff,
+  Waiter,
+  Receptionist,
+  KitchenStaff,
+}
+
+enum OrderStatus {
+  NewOrder,
+  AdditionalOrder,
+  FinishedOrder,
+  PartialOrder,
+}
+
+enum FoodItemStatus {
+  NotReady,
+  Ready,
+  Served,
+  NotAvailable,
 }
