d7371ef66208c9ef27230ceba491976a153f0e66
Shahil <shahil1.jha@gmail.com>
2021-05-18 21:27:51 +0545

    refactor: methdod private

--- lib/screens/receptionist/order_details_screen/local_widgets/receptionist_order_table.dart
+++ lib/screens/receptionist/order_details_screen/local_widgets/receptionist_order_table.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:summer_project/models/food_item.dart';
-import 'package:summer_project/utils/order_util.dart';
 import 'package:summer_project/widgets/app_table_components.dart';
 
 class ReceptionistOrderTable extends StatelessWidget {
   final List<FoodItem> list;
   const ReceptionistOrderTable({Key key, this.list}) : super(key: key);
+
+  List<TableRow> _getReceptionistTableRows(List<FoodItem> list) {
+    int index = 1;
+    List<TableRow> orderList = [];
+    for (var order in list) {
+      orderList.add(
+        TableRow(
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+          ),
+          children: [
+            AppDataCell(string: index.toString()),
+            AppDataCell(string: order.name),
+            AppDataCell(string: order.price.toString()),
+            AppDataCell(string: order.quantity.toString()),
+            AppDataCell(string: order.total.toString()),
+          ],
+        ),
+      );
+      index += 1;
+    }
+    return orderList;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +46,7 @@ class ReceptionistOrderTable extends StatelessWidget {
         AppHeaderCell(string: 'QTY'),
         AppHeaderCell(string: 'TOTAL'),
       ],
-      dataChildren: OrderUtil.getReceptionistTableRows(list),
+      dataChildren: _getReceptionistTableRows(list),
     );
   }
 }
